6b583bcbed36d9348a1181ddfc54f3df2ee2fd84
LeiMing627 <jackperry933jp@gmail.com>
2020-01-14 23:10:44 +0700

    Fix react native navigation

--- App.js
+++ App.js
@@ -7,25 +7,7 @@
  */
 
 import React, { Component } from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import Router from './src/routes'
-
+import Router from './src/routes';
 
 export default class App extends Component {
   render() {
