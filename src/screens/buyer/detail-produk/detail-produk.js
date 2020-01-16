f3192f816d9660f6e16892369c12c2f3a38728e0
LeiMing627 <jackperry933jp@gmail.com>
2020-01-16 22:16:03 +0700

    2. layout detail-produk, (need install (check trello), need add route)

--- src/screens/buyer/detail-produk/detail-produk.js
+++ src/screens/buyer/detail-produk/detail-produk.js
@@ -1,219 +1,245 @@
+//lib
 import React, {Component} from 'react';
-import {Text, View, TouchableOpacity, ScrollView} from 'react-native';
-import Search from '../../../components/search/search.js';
-import Footer from '../../../components/footer/footer.js';
-import styles from './home.style';
-import Category from '../../../components/category/category.js';
-import Border from '../../../components/border/border';
-import Card from '../../../components/card/card';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Dimensions,
+  StatusBar,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import {Text, Button, Tab, Thumbnail, Tabs} from 'native-base';
+import HeaderImageScrollView, {
+  TriggeringView,
+} from 'react-native-image-header-scroll-view';
+import {useHeaderHeight} from 'react-navigation-stack';
+import * as Animatable from 'react-native-animatable';
+import {BorderlessButton} from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-class HomeBuyer extends Component {
+//import
+import tvShowContent from '../../../assets/tvShowContent';
+import styles from './detail-produk.styles';
+import Card from '../../../components/card/card';
+
+const MIN_HEIGHT = useHeaderHeight.HEIGHT;
+const MAX_HEIGHT = 250;
+
+class DetailProdukBuyer extends Component {
+  constructor() {
+    super();
+    this.state = {showNavTitle: false};
+  }
+
   render() {
-    const {
-      container,
-      imageSlider,
-      readMoreImageSlider,
-      readMoreImageSliderText,
-      dotslider,
-      bottomImageSlider,
-      scroll,
-      category,
-      categoryContainer,
-      categoryContainerTop,
-      categoryContainerBot,
-    } = styles;
+    const data = {
+      title: 'Susu Ultra Full Cream',
+      dose: '1 pcs (1 liter)',
+      price: '40000',
+      producer: 'Mawang',
+      producerDesc:
+        'Mawang adalah seorang musisi asal Bandung yang belakangan ini sedang viral. Lagunya yang berjudul Kasih Sayang Kepada Orang Tua',
+      unit: '1 pcs',
+      description:
+        'Ultra Milk Rasa cocok untuk dikonsumsi sehari-hari dan juga diminum sehabis beraktivitas atau berolahraga untuk memenuhi kembali cairan tubuh dan membantu pembentukan tubuh. Produk ini baik untuk diminum anak-anak berumur 1 tahun ke atas, apalagi ditambah varian rasa dari bahan alami yang mengandung banyak nutrisi alami yang dibutuhkan tubuh, terutama bagi remaja yang sedang dalam masa pertumbuhan.',
+      label: 'Organic susu kuda',
+      label2: 'Produk Lokal',
+      healthAndBenefit:
+        'Dalam tiap 100 gram susu kuda liar menghasilkan 44 kalori, yang lebih sedikit ketimbang susu sapi yang sebesar 64 kalori sehingga membuat orang yang minum susu kuda tidak cepat gemuk.',
+      prepAndStorage:
+        'Kocok dahulu sebelum diminum jangan disimpan di dalam freezer dan jangan dimasak di atas panas api langsung. Kedua tindakan tersebut dapat menghilangkan khasiat susu kuda sumbawa asli',
+    };
+
     return (
-      <View style={container}>
-        <Search />
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <View style={imageSlider}>
-            <View style={bottomImageSlider}>
-              <TouchableOpacity style={readMoreImageSlider}>
-                <Text style={readMoreImageSliderText}> Lihat Semuanya</Text>
-              </TouchableOpacity>
+      <View style={{flex: 1}}>
+        <StatusBar barStyle="light-content" />
+        <HeaderImageScrollView
+          maxHeight={MAX_HEIGHT}
+          minHeight={MIN_HEIGHT}
+          maxOverlayOpacity={0.6}
+          minOverlayOpacity={0.3}
+          fadeOutForeground
+          renderHeader={() => (
+            <Image source={tvShowContent.image} style={styles2.image} />
+          )}
+          renderFixedForeground={() => (
+            <Animatable.View
+              style={styles2.navTitleView}
+              ref={navTitleView => {
+                this.navTitleView = navTitleView;
+              }}>
+              <Text style={styles.navTitle}>
+                {data.title}
+              </Text>
+            </Animatable.View>
+          )}
+          renderForeground={() => (
+            <View style={styles.titleContainer}>
+              <Text style={styles.imageTitle}>
+                {/* {tvShowContent.title} */}
+              </Text>
+            </View>
+          )}>
+          <TriggeringView
+            style={styles.section}
+            onHide={() => this.navTitleView.fadeInUp(200)}
+            onDisplay={() => this.navTitleView.fadeOut(100)}>
+            <Text style={styles.title}>{data.title}qweqew</Text>
+            <Text style={{fontSize: 14}}>{data.dose}</Text>
+            {/* ({tvShowContent.year}) */}
+          </TriggeringView>
+
+          <View style={styles.priceAndButton}>
+            <View style={styles.PABprice}>
+              <Text style={styles.price}>Rp {data.price}</Text>
+              <Text note style={{fontSize: 14}}>/ {data.unit}</Text>
+            </View>
+            <View style={styles.PABbutton}>
+              <Button small style={styles.PABbuttonA}>
+                <Text style={{fontWeight: 'bold'}}>Beli</Text>
+              </Button>
+
+              <Button small icon bordered style={styles.PABbuttonB}>
+                <Icon name="comments" style={styles.PABbuttonBicon} />
+                <Text style={{color: '#47A64A', fontWeight: 'bold'}}>Chat</Text>
+              </Button>
             </View>
           </View>
-          <View style={category}>
-            <Category
-              name="Sayuran Segar"
-              imgUri={require('../../../assets/icon/carrot.png')}
-            />
-            <Category
-              name="Buah Segar"
-              imgUri={require('../../../assets/icon/avocado.png')}
-            />
-            <Category
-              name="Sumber Karbohidrat"
-              imgUri={require('../../../assets/icon/wheat.png')}
-            />
-            <Category
-              name="Produk Terbaru"
-              imgUri={require('../../../assets/icon/new.png')}
-            />
-            <Category
-              name="Promo Spesial"
-              imgUri={require('../../../assets/icon/discount.png')}
-            />
-            <Category
-              name="Produk Terlaris"
-              imgUri={require('../../../assets/icon/star.png')}
-            />
-            <Category
-              name="Organik & Premium"
-              imgUri={require('../../../assets/icon/winner.png')}
-            />
-            <Category
-              name="Katering Sehat"
-              imgUri={require('../../../assets/icon/fruits.png')}
-            />
-            <Category
-              name="Grosir"
-              imgUri={require('../../../assets/icon/shoppingBasket.png')}
-            />
-            <Category
-              name="Makanan & Minuman"
-              imgUri={require('../../../assets/icon/food.png')}
-            />
-            <Category
-              name="Keperluan Dapur"
-              imgUri={require('../../../assets/icon/saltShaker.png')}
-            />
-            <Category
-              name="Daging & Seafood"
-              imgUri={require('../../../assets/icon/fish.png')}
-            />
-            <Category
-              name="Olahan Susu & Telur"
-              imgUri={require('../../../assets/icon/egg.png')}
-            />
+
+          <View style={styles.descriptionProductParent}>
+            <View style={styles.descriptionProductTitle}>
+              <Text style={styles.descriptionProductTitleText}>
+                {data.label}
+              </Text>
+              <Text style={{fontSize: 14}}>{data.label2}</Text>
+            </View>
+            <View style={styles.descriptionProductDesc}>
+              <Text style={{color: 'gray', fontSize: 14}}>{data.description}</Text>
+            </View>
           </View>
-          <Border />
-          <View style={categoryContainer}>
-            <View style={categoryContainerTop}>
-              <View>
-                <Text style={{fontSize: 18, fontWeight: 'bold'}}>
-                  Sayuran Segar
-                </Text>
-                <Text style={{fontSize: 12}}>Kumpulan Sayur Segar</Text>
-              </View>
-              <TouchableOpacity
-                style={{
-                  paddingLeft: 10,
-                  justifyContent: 'center',
-                }}>
-                <View
-                  style={{
-                    alignItems: 'center',
-                    flexDirection: 'row',
-                  }}>
-                  <Text
-                    style={{
-                      color: '#00B444',
-                      fontWeight: 'bold',
-                      paddingRight: 10,
-                    }}>
-                    Lihat Semua
+
+          <View style={styles.tabsParent}>
+            <Tabs
+              tabBarUnderlineStyle={styles.tabUnderLine}
+              backgroundColor="yellow"
+              tabContainerStyle="yellow">
+              <Tab
+                heading="Health and Benefits"
+                tabStyle={{backgroundColor: '#FEFEFE'}}
+                textStyle={{color: 'gray'}}
+                activeTabStyle={{backgroundColor: '#FEFEFE'}}
+                activeTextStyle={{color: 'black', fontWeight: 'normal'}}>
+                <View style={styles.tabsContent}>
+                  <Text style={styles.tabsContentText}>
+                    {data.healthAndBenefit}
                   </Text>
-                  <Icon name="chevron-right" size={12} color="#00B444" />
                 </View>
-              </TouchableOpacity>
-            </View>
-            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-              <View style={categoryContainerBot}>
-                <Card
-                  name="Bayam Hijau"
-                  label="Conventional"
-                  price="Rp 4.300"
-                  unit="250 gram"
-                />
-                <Card
-                  name="Labu Siam"
-                  label="Conventional"
-                  price="Rp 4.600"
-                  unit="500 gram"
-                />
-                <Card
-                  name="Paprika Kuning"
-                  label="Organik"
-                  price="Rp 20.000"
-                  unit="1 pcs"
-                />
-                <Card
-                  name="Wortel Organik"
-                  label="Organik"
-                  price="Rp 3.000"
-                  unit="500 gram"
-                />
-              </View>
-            </ScrollView>
-            <Border />
-            <View style={categoryContainer}>
-              <View style={categoryContainerTop}>
-                <View>
-                  <Text style={{fontSize: 18, fontWeight: 'bold'}}>
-                    Buah Segar
+              </Tab>
+
+              <Tab
+                heading="Prep and Storage"
+                tabStyle={{backgroundColor: '#FEFEFE'}}
+                textStyle={{color: 'gray'}}
+                activeTabStyle={{backgroundColor: '#FEFEFE'}}
+                activeTextStyle={{color: 'black', fontWeight: 'normal'}}>
+                <View style={styles.tabsContent}>
+                  <Text style={styles.tabsContentText}>
+                    {data.prepAndStorage}
                   </Text>
-                  <Text style={{fontSize: 12}}>Kumpulan Buah Segar</Text>
                 </View>
-                <TouchableOpacity
-                  style={{
-                    paddingLeft: 10,
-                    justifyContent: 'center',
-                  }}>
-                  <View
-                    style={{
-                      alignItems: 'center',
-                      flexDirection: 'row',
-                    }}>
-                    <Text
-                      style={{
-                        color: '#00B444',
-                        fontWeight: 'bold',
-                        paddingRight: 10,
-                      }}>
-                      Lihat Semua
-                    </Text>
-                    <Icon name="chevron-right" size={12} color="#00B444" />
-                  </View>
-                </TouchableOpacity>
+              </Tab>
+            </Tabs>
+          </View>
+
+          <View style={styles.producersParent}>
+            <Text style={{color: 'gray', paddingHorizontal: 20, fontSize: 16}}>
+              About the Producers
+            </Text>
+
+            <View style={styles.producersTitle}>
+              <View style={styles.producersAvatar}>
+                <Thumbnail
+                  source={{
+                    uri:
+                      'https://pbs.twimg.com/profile_images/1164752786992484354/PyFcqmzG.jpg',
+                  }}
+                />
               </View>
-              <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-                <View style={categoryContainerBot}>
-                  <Card
-                    name="Alpukat Mentega"
-                    label="Conventional"
-                    price="Rp 999"
-                    unit="1 kg"
-                  />
-                  <Card
-                    name="Pisang Raja"
-                    label="Conventional"
-                    price="Rp 29.800"
-                    unit="1 sisir"
-                  />
-                  <Card
-                    name="Pisang Uli"
-                    label="Organik"
-                    price="Rp 12.300"
-                    unit="1 sisir"
-                  />
-                  <Card
-                    name="Salak"
-                    label="Organik"
-                    price="Rp 4.990"
-                    unit="500 gram"
-                  />
-                </View>
-              </ScrollView>
-              <Border />
+              <View style={styles.producersName}>
+                <Text style={styles.producersNameText}>{data.producer}</Text>
+              </View>
+            </View>
+
+            <View style={styles.producersDesc}>
+              <Text style={styles.producersDescText}>{data.producerDesc}</Text>
             </View>
           </View>
-        </ScrollView>
-        <Footer />
+
+          
+<Text
+style={styles.relatedProductText}
+>
+  Related Products
+</Text>
+          
+
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            <View style={{flexDirection: 'row'}}>
+              <Card
+                name="Bayam Hijau"
+                label="Conventional"
+                price="Rp 4.300"
+                unit="250 gram"
+              />
+              <Card
+                name="Bayam Hijau"
+                label="Conventional"
+                price="Rp 4.300"
+                unit="250 gram"
+              />
+              <Card
+                name="Bayam Hijau"
+                label="Conventional"
+                price="Rp 4.300"
+                unit="250 gram"
+              />
+              <Card
+                name="Bayam Hijau"
+                label="Conventional"
+                price="Rp 4.300"
+                unit="250 gram"
+              />
+            </View>
+          </ScrollView>
+
+          <View
+          style={styles.end}
+          >
+
+          </View>
+
+          
+        </HeaderImageScrollView>
       </View>
     );
   }
 }
 
-export default HomeBuyer;
+export default DetailProdukBuyer;
+
+const styles2 = StyleSheet.create({
+  image: {
+    height: MAX_HEIGHT,
+    width: Dimensions.get('window').width,
+    alignSelf: 'stretch',
+    resizeMode: 'cover',
+  },
+  navTitleView: {
+    height: MIN_HEIGHT,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 16,
+    opacity: 0,
+  },
+});
